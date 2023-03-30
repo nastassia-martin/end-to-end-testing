8ff1345464effffb2b328a599ea37fcbfefe165f
nastassia martin <nastassia.martin@googlemail.com>
2023-03-30 15:36:07 +0200

    tests for search for mvie and get x numebr of hits

--- cypress/e2e/mostlyMundaneMovies.cy.ts
+++ cypress/e2e/mostlyMundaneMovies.cy.ts
@@ -4,44 +4,31 @@ describe('Mostly Mundane Movies', { defaultCommandTimeout: 10000 }, () => {
   })
   context('Movie Search', () => {
     it("Cant search without entering a title", () => {
-      // press / hit enter
-      // do not a title 
-      // a message should show up 
-      // message should stay there?
       cy.get('[type="submit"]').click()
       cy.get('.fade')
         .should('be.visible')
-        .should('have.class', 'alert')
-        .should('contain.html', 'div')
-        .and('contain.html', 'p')
-        .should('have.attr', 'role')
-      cy.get('.alert-heading')
-        .should('have.class', 'alert-heading h4')     //.should('have.text', ' You tried to search, good for you! 👀')
         .contains("Aww, that's cute")
       cy.get('p')
         .should('have.text', 'You tried to search, good for you! 👀')
     })
     it("Can't search without entering at least 3 characters", () => {
-      // go to search bar in form
-      // press / hit enter
-      // do not a title 
-      // a message should show up - very aggressive?==?????
-      // message should stay there?
       cy.get('.form-control').type('Us')
       cy.get('[type="submit"]').click()
       cy.get('.fade')
         .should('be.visible')
-        //.contains("Wow, that was stupid") ==> crash! 
-        .should('contain.html', 'div')
-        .and('contain.html', 'p')
-        .should('have.class', 'alert')
-        .should('have.attr', 'role')
-      cy.get('.alert-heading')
-        .should('have.class', 'alert-heading h4')
-        .contains("Wow, that was stupid")
+        .contains('Wow, that was stupid')
       cy.get('p')
         .should('have.text', 'Search query must be at least 3 characters long, duh ^^ 🙄')
     })
-    it.skip('Can search for "The Matrix" and get at least X number of hits')
+    it('Can search for "The Matrix" and get at least 3 number of hits', () => {
+      cy.get('.form-control').type('The Matrix')
+      cy.get('[type="submit"]').click()
+      cy.get('.movie-list')
+        .find(">.movie-list-item")
+        .should('have.length.at.least', 3)
+      cy.get('.card-title')
+        .should('contain', 'The Matrix')
+    })
+
   })
 })
