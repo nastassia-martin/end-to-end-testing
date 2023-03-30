dcf68563108ea3a432bd67863114f7357a6312ad
nastassia martin <nastassia.martin@googlemail.com>
2023-03-30 13:51:04 +0200

    tests added for cant search for movie with less than 3 characters

--- README.md
+++ README.md
@@ -26,7 +26,8 @@ E2E-testing med Cypress.
 ## Tests to run: 
 ### G
 [x] Can't search without entering a title
-[] Can't search without entering at least 3 characters
+[x] Can't search without entering at least 3 characters
+https://www.imdb.com/list/ls046446209/
 [] Can search for "The Matrix" and get at least X number of hits
 [] While searching, a loading spinner should appear
 [] Can click on the first search result (when the page has finished loading) and the page you end up on must match the ID of the film (via a data attribute)
--- cypress/e2e/mostlyMundaneMovies.cy.ts
+++ cypress/e2e/mostlyMundaneMovies.cy.ts
@@ -3,8 +3,7 @@ describe('Mostly Mundane Movies', { defaultCommandTimeout: 10000 }, () => {
     cy.visit('/')
   })
   context('Movie Search', () => {
-    it('Cant search without entering a title', () => {
-      // go to search bar in form
+    it("Cant search without entering a title", () => {
       // press / hit enter
       // do not a title 
       // a message should show up 
@@ -12,15 +11,37 @@ describe('Mostly Mundane Movies', { defaultCommandTimeout: 10000 }, () => {
       cy.get('[type="submit"]').click()
       cy.get('.fade')
         .should('be.visible')
-        // should have 2 children
         .should('have.class', 'alert')
-        .should('have.descendants', 'div')
-        .should('have.descendants', 'p')
+        .should('contain.html', 'div')
+        .and('contain.html', 'p')
+        .should('have.attr', 'role')
       cy.get('.alert-heading')
         .should('have.class', 'alert-heading h4')     //.should('have.text', ' You tried to search, good for you! 👀')
         .contains("Aww, that's cute")
       cy.get('p')
         .should('have.text', 'You tried to search, good for you! 👀')
     })
+    it("Can't search without entering at least 3 characters", () => {
+      // go to search bar in form
+      // press / hit enter
+      // do not a title 
+      // a message should show up - very aggressive?==?????
+      // message should stay there?
+      cy.get('.form-control').type('Us')
+      cy.get('[type="submit"]').click()
+      cy.get('.fade')
+        .should('be.visible')
+        //.contains("Wow, that was stupid") ==> crash! 
+        .should('contain.html', 'div')
+        .and('contain.html', 'p')
+        .should('have.class', 'alert')
+        .should('have.attr', 'role')
+      cy.get('.alert-heading')
+        .should('have.class', 'alert-heading h4')
+        .contains("Wow, that was stupid")
+      cy.get('p')
+        .should('have.text', 'Search query must be at least 3 characters long, duh ^^ 🙄')
+    })
+    it.skip('Can search for "The Matrix" and get at least X number of hits')
   })
 })
